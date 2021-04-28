5abe41bc03792f826983faa5cc0b09c2f155bc97
Necther Louie Souribio <nectherlouie@gmail.com>
2021-04-28 01:07:47 +0100

    Added a dev road map with task lists

---
+++ Assets/Micro/Scripts/Notes/DevelopmentRoadmap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MB
+{
+    [Serializable]
+    public class DevelopmentNote
+    {
+        public enum TaskType { BACKLOG, TODO, DOING, DONE };
+
+        public TaskType type;
+        public string text;
+    }
+
+    public class DevelopmentRoadmap : MonoBehaviour
+    {
+        public List<DevelopmentNote> devNotes = new List<DevelopmentNote>();
+    }
+}
